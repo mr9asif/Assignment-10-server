57546865e49a4431ae745d19f1fc79aab4b89c48
mr9 asif <asifalibd002@gmail.com>
2024-04-28 12:26:16 +0600

    added list delete and more

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const cors = require('cors')
 require('dotenv').config()
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const port = process.env.PORT || 2000;
 
 const app = express();
-
+const cors = require('cors')
 // middle ware
 app.use(cors());
+app.use(express.json())
 
 // get here
 app.get('/', (req,res)=>{
     res.send('sever is running...')
 })
-console.log(process.env.DB_PASS)
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p7hqbnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,24 +35,83 @@ async function run() {
     const TravelesCollection = database.collection("Traveles");
 
 //    Multiple item post
-       app.post('/tourists', async(req, res)=>{
-        const Tourist = {
-          title: "Record of a Shriveled Datum",
-          content: "No bytes, no problem. Just insert a document, in MongoDB",
-        }
-          const options = { ordered: true };
-          const result = await foods.insertOne(Tourist, options);
+       app.post('/addTourists', async(req, res)=>{
+        console.log(req.body)
+        const tourist = req.body;
+          
+       
+          const result = await TravelesCollection.insertOne(tourist);
           res.send(result)
+          console.log(result)
        })
 
+      //  put
+      app.put('/update/:id', async(req,res)=>{
+         console.log(req.params.id)
+         const id= req.body.id;
+         const query = {_id: new ObjectId(id)}
+         const updateSport={
+           $set:{
+            touristSpotName:req.body.touristSpotName,
+            countryName:req.body.countryName,
+            location:req.body.location,
+            description:req.body.description,
+            averageCost:req.body.averageCost,
+            seasonality:req.body.seasonality,
+            travelTime:req.body.travelTime,
+            totalvisitor:req.body.totalvisitor,
+           }
+           
+         }
+         const result = await TravelesCollection.updateOne( query,updateSport)
+         res.send(result)
+         console.log(result)
+      })
 
     //    get
-     app.get('/tourists', async(req, res)=>{
+     app.get('/addTourists', async(req, res)=>{
         const cursor = TravelesCollection.find();
         const result = await cursor.toArray();
         res.send(result)
+        console.log(result)
      })
 
+    //  single tourist
+    app.get('/viewDetails/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log(id)
+      const query = {_id: new ObjectId(id)}
+       const result = await TravelesCollection.findOne(query);
+       console.log(result)
+       res.send(result)
+    })
+
+    // DELETE
+    app.delete('/delete/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query={_id: new ObjectId(id)};
+        const result=await TravelesCollection.deleteOne(query);
+        res.send(result)
+    })
+
+    // update
+    app.get('/update/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log(id)
+      const query = {_id: new ObjectId(id)}
+       const result = await TravelesCollection.findOne(query);
+       console.log(result)
+       res.send(result)
+    })
+
+     app.get('/myList/:email', async(req, res)=>{
+        console.log(req.params)
+       
+        const query = {userEmail:  req.params.email}
+        const result = await TravelesCollection.find(query).toArray()
+        res.send(result)
+        console.log(result)
+     })
 
 
 
@@ -74,3 +134,5 @@ app.listen(port, ()=>{
     console.log(`server is running ..${port}`)
 })
 
+// QGcOjw6mLlLsVg0Y
+// Traveler
